a23cc12890ac72389134dbc3bcd7f8d9de35913b
lohkung@gmail.com <lohkung@gmail.com>
2018-06-28 17:49:23 +0700

    add context,item

---
+++ RestWebAPICore/Models/CreditCardContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestWebAPICore.Models
+{
+    public class CreditCardContext : DbContext 
+    {
+        public CreditCardContext(DbContextOptions<CreditCardContext> options) : base (options)
+        {
+
+        }
+        public DbSet<CreditCardItem> creditCardItems { get; set; }
+
+    }
+}
---
+++ RestWebAPICore/Models/CreditCardItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWebAPICore.Models
+{
+    public class CreditCardItem
+    {
+        public long Id { get; set; }
+        public String CardNumber { get; set; }
+    }
+}
