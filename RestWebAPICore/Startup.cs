910cd6cde9ca059e6b7fd6a9f70df8280e996b7b
lohkungByAssary <lohkung@gmail.com>
2018-07-04 01:39:21 +0700

    Add Swagger

--- RestWebAPICore/Startup.cs
+++ RestWebAPICore/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using RestWebAPICore.Models;
 using Microsoft.EntityFrameworkCore;
 using System.IO;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace RestWebAPICore
 {
@@ -24,12 +25,24 @@ namespace RestWebAPICore
          
             services.AddDbContext<CreditCardContext>(opt =>
                 opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "CheckCreditCard API", Version = "v1" , Description = "Swagger Core API"});
+            });
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseSwagger();
+
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CheckCreditCard API");
+                c.RoutePrefix = string.Empty
+            });
             app.UseMvc();
         }
     }
